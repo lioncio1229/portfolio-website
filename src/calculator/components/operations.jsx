8256d7613400a0790a696adb7f585f21d6186004
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2022-08-31 21:35:39 +0800

    Added Operations - Calculator

---
+++ src/calculator/components/operations.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Box, List, ListItemButton, ListItemText, ListItemIcon} from "@mui/material";
+import {Add, Remove, Close, Percent} from "@mui/icons-material";
+
+const ListItemCustom = ({icon, selected, index, onClick, children}) => {
+    return (
+        <ListItemButton selected={selected === index} onClick={onClick} sx={{color : 'primary.main'}}>
+            <ListItemIcon>
+                {icon}
+            </ListItemIcon>
+            <ListItemText primary={children} />
+        </ListItemButton>
+    )
+}
+
+const renderObj = [
+    { index : 0, title : 'Addition', icon : <Add/> },
+    { index : 1, title : 'Subtraction', icon : <Remove/> },
+    { index : 2, title : 'Multiplication', icon : <Close/> },
+    { index : 3, title : 'Division', icon : <Percent/> },
+]
+
+const Operations = () => {
+    const [index, setIndex] = React.useState(0);
+
+    return (
+      <Box sx={{ width: "100%", bgcolor: "background.paper" }}>
+        <List component="nav">
+        {
+            renderObj.map(obj => (
+                <ListItemCustom
+                    icon={obj.icon}
+                    selected={index}
+                    index={obj.index}
+                    onClick={() => setIndex(obj.index)}
+                >
+                    {obj.title}
+                </ListItemCustom>
+            ))
+        }
+
+        </List>
+      </Box>
+    );
+}
+ 
+export default Operations;
--- src/calculator/index.jsx
+++ src/calculator/index.jsx
@@ -1,10 +1,12 @@
 import './style.css';
 import Main from "./main";
+import Operations from './components/operations';
 
 const Calculatorapp = () => {
   return (
     <>
       <Main />
+      <Operations />
     </>
   );
 };
