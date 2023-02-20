729b6c26c4b64735b8348fc7693a3e44ea7f777d
Lioncio Morcilla <lionciomorcilla12@gmail.com>
2023-02-20 18:17:38 +0800

    Refactor layout

--- src/components/job-experience/JobExperience.jsx
+++ src/components/job-experience/JobExperience.jsx
@@ -23,58 +23,55 @@ const TextWithIcon = ({ text, icon, isActive }) => (
 );
 
 const Item = ({ title, description, company, location, jobType, workDuration, logo, isActive }) => (
-  <Stack rowGap={2}>
-    <Stack
-      direction="row"
-      justifyContent="space-between"
-      height={65}
-      marginTop={2}
-    >
-      <Stack direction="row">
+  <Stack rowGap={1}>
+        <Stack direction="row" mt={2}>
+            <Box
+            sx={{
+                width: 65,
+                height: 65,
+                bgcolor: "white",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+            >
+                {
+                    logo ? 
+                    <img src={logo} alt="" width='100%' height='100%' />:
+                    <CropOriginal color="primary" sx={{ width: "60%", height: "60%" }} />
+                }
+            </Box>
+            <Stack width='100%'>
+                <Stack
+                direction="row"
+                justifyContent="space-between"
+                >
+                    <Stack justifyContent="space-between">
+                        <Typography color="primary" sx={{ fontSize: 23 }}>
+                            {title}
+                        </Typography>
+                        <Stack direction="row" justifyContent="space-between">
+                            {company && <TextWithIcon text={company} icon={<Business />} />}
+                            {location && <TextWithIcon text={location} icon={<LocationOn />} />}
+                        </Stack>
+                    </Stack>
 
-        <Box
-          sx={{
-            width: 65,
-            height: 65,
-            bgcolor: "white",
-            mr: 1.5,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-            {
-                logo ? 
-                <img src={logo} alt="" width='100%' height='100%' />:
-                <CropOriginal color="primary" sx={{ width: "60%", height: "60%" }} />
-            }
-        </Box>
-
-        <Stack justifyContent="space-between">
-          <Typography color="primary" sx={{ fontSize: 23 }}>
-            {title}
-          </Typography>
-          <Stack direction="row" justifyContent="space-between">
-            {company && <TextWithIcon text={company} icon={<Business />} />}
-            {location && <TextWithIcon text={location} icon={<LocationOn />} />}
-          </Stack>
+                    <Stack justifyContent="space-between" alignItems="flex-end">
+                        {jobType && <Chip label={jobType} color="primary" sx={{ width: 120 }} />}
+                        {workDuration && <TextWithIcon text={workDuration} isActive={isActive} icon={<CalendarMonth />} />}
+                    </Stack>
+                </Stack>
+                {
+                    description && 
+                    <Box sx={{bgcolor: 'white', borderRadius: 3, p: 2}}>
+                        <Typography color="primary" sx={{ fontSize: 15 }}>
+                            {description}
+                        </Typography>
+                    </Box>
+                }
+            </Stack>
         </Stack>
-      </Stack>
-
-      <Stack justifyContent="space-between" alignItems="flex-end">
-        {jobType && <Chip label={jobType} color="primary" sx={{ width: 120 }} />}
-        {workDuration && <TextWithIcon text={workDuration} isActive={isActive} icon={<CalendarMonth />} />}
-      </Stack>
-    </Stack>
-    {
-        description && 
-        <Box sx={{bgcolor: 'white', ml:'80px', borderRadius: 3, p: 2}}>
-            <Typography color="primary" sx={{ fontSize: 15 }}>
-                {description}
-            </Typography>
-        </Box>
-    }
-    <Divider />
+        <Divider />
   </Stack>
 );
 
